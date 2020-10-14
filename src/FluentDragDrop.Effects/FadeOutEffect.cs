a839f84322f50cab04a0cb668eae3f22a3177a74
Andreas Wäscher <awaescher@sodacore.net>
2020-10-15 00:03:11 +0200

    Add a first set of default effects to FluentDragDrop.Effects

---
+++ src/FluentDragDrop.Effects/FadeOutEffect.cs
@@ -0,0 +1,34 @@
+﻿using FluentTransitions;
+using FluentTransitions.Methods;
+using System;
+
+namespace FluentDragDrop.Effects
+{
+	/// <summary>
+	/// A fade-out effect fading the opacity of the preview image from its current value to zero
+	/// </summary>
+	public class FadeOutEffect : DefaultEndEffect
+	{
+		/// <summary>
+		/// Starts the effect with the given arguments
+		/// </summary>
+		/// <param name="arguments">The effect arguments containing information about the preview form and the affected controls</param>
+		public override void Start(IEffect.Arguments arguments)
+		{
+			var transition = Transition
+				.With(arguments.PreviewForm, nameof(arguments.PreviewForm.Opacity), 0d)
+				.Build(new Deceleration(500));
+
+			void selfRemovingHandler(object _, Transition.Args __)
+			{
+				transition.TransitionCompletedEvent -= selfRemovingHandler;
+
+				arguments.PreviewForm.BeginInvoke((Action)(() => base.ClosePreview(arguments)));
+			}
+
+			transition.TransitionCompletedEvent += selfRemovingHandler;
+
+			transition.Run();
+		}
+	}
+}
---
+++ src/FluentDragDrop.Effects/ReturnToStartEffect.cs
@@ -0,0 +1,41 @@
+﻿using FluentDragDrop.Preview;
+using FluentTransitions;
+using FluentTransitions.Methods;
+using System;
+using System.Drawing;
+
+namespace FluentDragDrop.Effects
+{
+	/// <summary>
+	/// An advanced effect returning the preview image to its initial location while fading it out
+	/// </summary>
+	public class ReturnToStartEffect : DefaultEndEffect
+	{
+		/// <summary>
+		/// Starts the effect with the given arguments
+		/// </summary>
+		/// <param name="arguments">The effect arguments containing information about the preview form and the affected controls</param>
+		public override void Start(IEffect.Arguments arguments)
+		{
+			var controlLocation = arguments.SourceControl.PointToScreen(Point.Empty);
+			var originalOpacity = arguments.PreviewForm.Opacity;
+
+			var returnTransition = Transition
+				.With(arguments.PreviewForm, nameof(arguments.PreviewForm.Left), controlLocation.X)
+				.With(arguments.PreviewForm, nameof(arguments.PreviewForm.Top), controlLocation.Y)
+				.With(arguments.PreviewForm, nameof(arguments.PreviewForm.Opacity), 0d)
+				.Build(new Deceleration(500));
+
+			void selfRemovingHandler(object _, Transition.Args __)
+			{
+				returnTransition.TransitionCompletedEvent -= selfRemovingHandler;
+
+				arguments.PreviewForm.BeginInvoke((Action)(() => base.ClosePreview(arguments)));
+			}
+
+			returnTransition.TransitionCompletedEvent += selfRemovingHandler;
+
+			Transition.RunChain(returnTransition);
+		}
+	}
+}
