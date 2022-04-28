0c56f5c3ed0ac598caf57fa2441a725dda4da1d1
Andreas Wäscher <awaescher@sodacore.net>
2022-04-28 11:39:08 +0200

    Introduce new OverlayForm

--- src/FluentDragDrop/Preview/PreviewForm.cs
+++ src/FluentDragDrop/Preview/PreviewForm.cs
@@ -5,46 +5,20 @@ using System.Windows.Forms;
 
 namespace FluentDragDrop.Preview
 {
-	internal sealed class PreviewForm : Form
+	internal class PreviewForm : OverlayForm
 	{
-		[System.Runtime.InteropServices.DllImport("user32.dll")]
-		private static extern IntPtr SetActiveWindow(IntPtr handle);
-
-		private const int WM_NCHITTEST = 0x84;
-		private const int WM_ACTIVATE = 6;
-		private const int WM_CAPTURECHANGED = 0x215;
-		private const int WM_MOUSEACTIVATE = 0x0021;
-
-		private const int MA_NOACTIVATEANDEAT = 0x0004;
-
-		private const int WA_INACTIVE = 0;
-
-		private const int WS_EX_NOACTIVATE = 0x08000000;
-		private const int WS_EX_TOPMOST = 0x00000008;
-
-		private const int HTTRANSPARENT = -1;
 		private Effects.Effects _effects;
 
 		public PreviewForm()
 		{
-			SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-			SetStyle(ControlStyles.Selectable, false);
-
-			Hide();
-
-			IsDragging = false;
 			MinimumSize = Size.Empty;
 			Size = Size.Empty;
-			StartPosition = FormStartPosition.Manual;
-			ShowInTaskbar = false;
-			Location = new Point(-9999, -9999);
+			Location = new Point(-5000, -5000);
 			Enabled = false;
-			TabStop = false;
-			BackColor = Color.Transparent;
-			TopMost = true;
-			FormBorderStyle = FormBorderStyle.None;
 		}
+
+
+
 
 		public void Start(Control sourceControl, Effects.Effects effects, Point location, IPreview preview)
 		{
@@ -126,52 +100,6 @@ namespace FluentDragDrop.Preview
 				Location = location;
 		}
 
-		protected override CreateParams CreateParams
-		{
-			get
-			{
-				CreateParams param = base.CreateParams;
-				param.ExStyle |= WS_EX_TOPMOST; // make the form topmost
-				param.ExStyle |= WS_EX_NOACTIVATE; // prevent the form from being activated
-				return param;
-			}
-		}
-
-		protected override void WndProc(ref Message m)
-		{
-			switch (m.Msg)
-			{
-				case WM_MOUSEACTIVATE:
-					// prevent the form from being clicked and gaining focus
-					m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
-					return;
-				case WM_ACTIVATE:
-					// if a message gets through to activate the form somehow
-					if (((int)m.WParam & 0xFFFF) != WA_INACTIVE)
-					{
-						if (m.LParam != IntPtr.Zero)
-							SetActiveWindow(m.LParam);
-						else
-							SetActiveWindow(IntPtr.Zero); // Could not find sender, just in-activate it.
-					}
-					break;
-				case WM_NCHITTEST:
-					m.Result = new IntPtr(HTTRANSPARENT);
-					return;
-				case WM_CAPTURECHANGED:
-					if (m.LParam != base.Handle)
-						FormCaptureLost();
-					break;
-			}
-
-			base.WndProc(ref m);
-		}
-
-		private void FormCaptureLost()
-		{
-			Stop(null, wasCancelled: true);
-		}
-
 		protected override void OnPaintBackground(PaintEventArgs e)
 		{
 			// DO NOT paint the background to prevent flickering
@@ -190,6 +118,12 @@ namespace FluentDragDrop.Preview
 				BeginInvoke((Action)InvalidatePreview);
 			else
 				InvalidatePreview();
+		}
+
+
+		protected override void OnLostCapture()
+		{
+			Stop(null, wasCancelled: true);
 		}
 
 		private bool IsDragging { get; set; }
